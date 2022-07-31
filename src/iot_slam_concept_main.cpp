9844e8c34362861404b01c02d9bd7e933152dc47
alex <adthoms@uwaterloo.ca>
2022-07-30 17:49:16 -0700

    initial refactoring for multi-session SLAM

--- src/iot_slam_concept_main.cpp
+++ src/iot_slam_concept_main.cpp
@@ -21,102 +21,112 @@ int main(int argc, char* argv[]) {
   GaussianDistribution odometry_dist(0, ODOMETRY_NOISE);
   GaussianDistribution wireless_dist(0, WIRELESS_NOISE);
 
-  // generate ground truth and measured robot positions
-  gtsam::Pose3 init_pose =
-      gtsam::Pose3(gtsam::Rot3::Ypr(0, 0, 0), gtsam::Point3(10, 0, 0));
-  gtsam::Pose3 delta_pose =
-      gtsam::Pose3(gtsam::Rot3::Ypr(-M_PI / 220, 0, 0),
-                   gtsam::Point3(sin(M_PI / 22), sin(M_PI / 22), 0));
-  auto ground_truth_robot_poses =
-      GeneratePoses(init_pose, delta_pose, ODOMETRY_STEPS);
-  auto measured_robot_poses =
-      GeneratePoses(init_pose, delta_pose, ODOMETRY_STEPS, ODOMETRY_NOISE);
+  // generate SLAM sessions for each floor
+  std::vector<double> init_z{1, 11};
+  for (const auto& [floor_id, floor_anchor_association] :
+       floor_anchor_association_map) {
+    // generate ground truth and measured robot positions
+    gtsam::Pose3 init_pose = gtsam::Pose3(
+        gtsam::Rot3::Ypr(0, 0, 0), gtsam::Point3(10, 0, init_z.at(floor_id)));
+    gtsam::Pose3 delta_pose =
+        gtsam::Pose3(gtsam::Rot3::Ypr(-M_PI / 220, 0, 0),
+                     gtsam::Point3(sin(M_PI / 22), sin(M_PI / 22), 0));
+    auto ground_truth_robot_poses =
+        GeneratePoses(init_pose, delta_pose, ODOMETRY_STEPS);
+    auto measured_robot_poses =
+        GeneratePoses(init_pose, delta_pose, ODOMETRY_STEPS, ODOMETRY_NOISE);
 
-  // output to txt files
-  PrintPoses(ground_truth_robot_poses,
-             full_file_path + "ground_truth_robot_poses.txt");
-  PrintPoses(measured_robot_poses, full_file_path + "measured_robot_poses.txt");
+    // output to txt files
+    PrintPoses(ground_truth_robot_poses, full_file_path +
+                                             "ground_truth_robot_poses_" +
+                                             std::to_string(floor_id) + ".txt");
+    PrintPoses(measured_robot_poses, full_file_path + "measured_robot_poses_" +
+                                         std::to_string(floor_id) + ".txt");
 
-  // Instantiate graph
-  gtsam::ExpressionFactorGraph graph;
+    // Instantiate graph
+    gtsam::ExpressionFactorGraph graph;
 
-  // Add prior factor to first robot position
-  graph.addExpressionFactor(gtsam::Point3_('x', 0),
-                            ground_truth_robot_poses.at(0).translation(),
-                            prior_noise);
+    // Add prior factor to first robot position
+    graph.addExpressionFactor(gtsam::Point3_('x', 0),
+                              ground_truth_robot_poses.at(0).translation(),
+                              prior_noise);
 
-  // Simulate measurements from robot to anchors and odometry constraints
-  for (size_t i = 0; i < ground_truth_robot_poses.size(); ++i) {
-    const gtsam::Point3& t_WORLD_ROBOT =
-        ground_truth_robot_poses.at(i).translation();
+    // Simulate measurements from robot to anchors and odometry constraints
+    for (size_t i = 0; i < ground_truth_robot_poses.size(); ++i) {
+      const gtsam::Point3& t_WORLD_ROBOT =
+          ground_truth_robot_poses.at(i).translation();
+
+      for (const auto& [anchor_id, anchor_position] : anchor_position_map) {
+        // perturb anchor point for noisy robot to anchor measurement
+        gtsam::Point3 t_WORLD_ANCHOR_PERTURB(anchor_position);
+        PerturbPoint(t_WORLD_ANCHOR_PERTURB, wireless_dist.GetRandomValue());
+
+        // add robot to anchor constraint
+        graph.addExpressionFactor(
+            between(gtsam::Point3_('x', i), gtsam::Point3_('a', anchor_id)),
+            t_WORLD_ROBOT.between(t_WORLD_ANCHOR_PERTURB), wireless_noise);
+      }
+
+      if (i > 0) {
+        // add odometry constraints
+        graph.addExpressionFactor(
+            between(gtsam::Point3_('x', i - 1), gtsam::Point3_('x', i)),
+            measured_robot_poses.at(i - 1).translation().between(
+                measured_robot_poses.at(i).translation()),
+            odometry_noise);
+      }
+    }
+
+    // provide initial estimates using prior information
+    gtsam::Values initial;
+    for (size_t i = 0; i < measured_robot_poses.size(); ++i) {
+      initial.insert<gtsam::Point3>(gtsam::Symbol('x', i),
+                                    measured_robot_poses.at(i).translation());
+    }
 
     for (const auto& [anchor_id, anchor_position] : anchor_position_map) {
-      // perturb anchor point for noisy robot to anchor measurement
       gtsam::Point3 t_WORLD_ANCHOR_PERTURB(anchor_position);
       PerturbPoint(t_WORLD_ANCHOR_PERTURB, wireless_dist.GetRandomValue());
-
-      // add robot to anchor constraint
-      graph.addExpressionFactor(
-          between(gtsam::Point3_('x', i), gtsam::Point3_('a', anchor_id)),
-          t_WORLD_ROBOT.between(t_WORLD_ANCHOR_PERTURB), wireless_noise);
+      initial.insert<gtsam::Point3>(gtsam::Symbol('a', anchor_id),
+                                    t_WORLD_ANCHOR_PERTURB);
     }
 
-    if (i > 0) {
-      // add odometry constraints
-      graph.addExpressionFactor(
-          between(gtsam::Point3_('x', i - 1), gtsam::Point3_('x', i)),
-          measured_robot_poses.at(i - 1).translation().between(
-              measured_robot_poses.at(i).translation()),
-          odometry_noise);
+    // for (const auto& [tag_id, tag_position] : tag_position_map) {
+    //   initial.insert<gtsam::Point3>(gtsam::Symbol('t', tag_id),
+    //   tag_position);
+    // }
+
+    // optimize graph
+    gtsam::Values result =
+        gtsam::LevenbergMarquardtOptimizer(graph, initial).optimize();
+
+    // print results
+    std::vector<gtsam::Point3> optimized_robot_points;
+    for (size_t i = 0; i < ground_truth_robot_poses.size(); ++i) {
+      optimized_robot_points.push_back(
+          result.at<gtsam::Point3>(gtsam::Symbol('x', i)));
     }
+    PrintPoints(optimized_robot_points, full_file_path +
+                                            "optimized_robot_points_" +
+                                            std::to_string(floor_id) + ".txt");
+
+    NodePositionMap optimized_anchor_position_map;
+    for (const auto& [anchor_id, anchor_position] : anchor_position_map) {
+      optimized_anchor_position_map[anchor_id] =
+          result.at<gtsam::Point3>(gtsam::Symbol('a', anchor_id));
+    }
+    PrintMap(optimized_anchor_position_map,
+             full_file_path + "optimized_anchor_position_map_" +
+                 std::to_string(floor_id) + ".txt");
+
+    // NodePositionMap optimized_tag_position_map;
+    // for (const auto& [tag_id, tag_position] : tag_position_map) {
+    //   optimized_anchor_position_map[tag_id] =
+    //       result.at<gtsam::Point3>(gtsam::Symbol('t', tag_id));
+    // }
+    // PrintMap(optimized_tag_position_map,
+    //          full_file_path + "optimized_tag_position_map.txt");
   }
-
-  // provide initial estimates using prior information
-  gtsam::Values initial;
-  for (size_t i = 0; i < measured_robot_poses.size(); ++i) {
-    initial.insert<gtsam::Point3>(gtsam::Symbol('x', i),
-                                  measured_robot_poses.at(i).translation());
-  }
-
-  for (const auto& [anchor_id, anchor_position] : anchor_position_map) {
-    gtsam::Point3 t_WORLD_ANCHOR_PERTURB(anchor_position);
-    PerturbPoint(t_WORLD_ANCHOR_PERTURB, wireless_dist.GetRandomValue());
-    initial.insert<gtsam::Point3>(gtsam::Symbol('a', anchor_id),
-                                  t_WORLD_ANCHOR_PERTURB);
-  }
-
-  // for (const auto& [tag_id, tag_position] : tag_position_map) {
-  //   initial.insert<gtsam::Point3>(gtsam::Symbol('t', tag_id), tag_position);
-  // }
-
-  // optimize graph
-  gtsam::Values result =
-      gtsam::LevenbergMarquardtOptimizer(graph, initial).optimize();
-
-  // print results
-  std::vector<gtsam::Point3> optimized_robot_points;
-  for (size_t i = 0; i < ground_truth_robot_poses.size(); ++i) {
-    optimized_robot_points.push_back(
-        result.at<gtsam::Point3>(gtsam::Symbol('x', i)));
-  }
-  PrintPoints(optimized_robot_points,
-              full_file_path + "optimized_robot_points.txt");
-
-  NodePositionMap optimized_anchor_position_map;
-  for (const auto& [anchor_id, anchor_position] : anchor_position_map) {
-    optimized_anchor_position_map[anchor_id] =
-        result.at<gtsam::Point3>(gtsam::Symbol('a', anchor_id));
-  }
-  PrintMap(optimized_anchor_position_map,
-           full_file_path + "optimized_anchor_position_map.txt");
-
-  // NodePositionMap optimized_tag_position_map;
-  // for (const auto& [tag_id, tag_position] : tag_position_map) {
-  //   optimized_anchor_position_map[tag_id] =
-  //       result.at<gtsam::Point3>(gtsam::Symbol('t', tag_id));
-  // }
-  // PrintMap(optimized_tag_position_map,
-  //          full_file_path + "optimized_tag_position_map.txt");
 
   return 0;
 }
